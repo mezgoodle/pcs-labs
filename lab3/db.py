3422b8c93e6b35a960c2fcb3c8e2a6211d61d591
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-03-27 10:20:58 +0300

    Try to insert a document

--- lab3/db.py
+++ lab3/db.py
@@ -1,5 +1,6 @@
 # connect to the mongodb in the docker container
 
+import datetime
 import os
 
 import pymongo
@@ -11,12 +12,19 @@ def get_db():
     # connect to the mongodb
     client = pymongo.MongoClient(MONGODB_URI)
     # get the database
-    db = client.get_default_database()
+    db = client.example
     return db
 
 
 # do some testing
 if __name__ == "__main__":
     db = get_db()
-    print(db)
-    print(db.list_collection_names())
+    post = {
+        "author": "Mike",
+        "text": "My first blog post!",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.utcnow(),
+    }
+    posts = db.posts
+    post_id = posts.insert_one(post).inserted_id
+    print(post_id)
