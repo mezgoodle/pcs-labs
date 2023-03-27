5d47451e62ba96d56a02ae074a2e414fbb778bed
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-03-27 10:34:44 +0300

    Add cafes

--- lab3/db.py
+++ lab3/db.py
@@ -7,24 +7,37 @@ import pymongo
 
 
 def get_db():
-    # get the mongodb connection string
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb://localhost:27017")
-    # connect to the mongodb
-    client = pymongo.MongoClient(MONGODB_URI, username="root", password="example")
-    # get the database
-    db = client.example
+    client = pymongo.MongoClient(MONGODB_URI, username="root", password="root")
+    db = client.cafes
     return db
 
 
 # do some testing
 if __name__ == "__main__":
     db = get_db()
-    post = {
-        "author": "Mike",
-        "text": "My first blog post!",
-        "tags": ["mongodb", "python", "pymongo"],
-        "date": datetime.datetime.utcnow(),
-    }
-    posts = db.posts
-    post_id = posts.insert_one(post).inserted_id
-    print(post_id)
+    cafes = db.cafes
+    new_cafes = [
+        {
+            "name": "Starbucks",
+            "location": "New York",
+            "rating": 4.5,
+        },
+        {
+            "name": "Cafe Coffee Day",
+            "location": "Bangalore",
+            "rating": 4.0,
+        },
+        {
+            "name": "Dunkin Donuts",
+            "location": "New York",
+            "rating": 4.0,
+        },
+        {
+            "name": "Tim Hortons",
+            "location": "Toronto",
+            "rating": 4.0,
+        },
+    ]
+    result = cafes.insert_many(new_cafes)
+    result.inserted_ids
